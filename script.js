882d73121f87d43c456f036d5f70af0e5bd75e87
javad-fp <javad_fazelpour@outlook.com>
2025-02-02 16:18:25 +0330

    Add Book class nad library array
    - The Book class is a model of a real book
    - The library array stores book objects
    - There is a displayBooks function that prints information of the books
      that are stored in the library to the browser console

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
+
+function displayBooks(library) {
+  for (let index = 0; index < library.length; index++) {
+    console.log(library[index].info());
+  }
+}
